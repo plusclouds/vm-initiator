94defcca264ec1a44fcd897c0ef27cf0756dd9cb
Harun Barış Bulut <baris.bulut@plusclouds.com>
2023-11-15 23:01:11 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-
-# We are updating the password of the server here.
 URL=$(printenv PLUSCLOUDS_API)
 SERIAL=$(dmidecode -s system-serial-number)
 META=$(curl -s $URL/v2/iaas/virtual-machines/meta-data?uuid=$SERIAL)
+
 PASSWORD=$(echo $META | jq -r '.data.password')
+HOSTNAME=$(echo $META | jq -r '.data.hostname')
+
+#Changing password
 echo root:$PASSWORD | chpasswd
+
+#Changing the hostname
+hostnamectl set-hostname $HOSTNAME
