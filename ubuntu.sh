5a3e28d45620ad5129d9dc8f86e750d7cfcd3275
Harun Barış Bulut <baris.bulut@plusclouds.com>
2023-11-16 09:54:00 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 URL=$(printenv LEO_API)
 SERIAL=$(dmidecode -s system-serial-number)
-META=$(curl -s $URL/v2/iaas/virtual-machines/meta-data?runner=bash&version=1&uuid=$SERIAL)
+META=$(curl -s $URL/v2/iaas/virtual-machines/meta-data?uuid=$SERIAL&runner=bash&version=1)
 
 PASSWORD=$(echo $META | jq -r '.data.password')
 HOSTNAME=$(echo $META | jq -r '.data.hostname')
