37cbd9d9c2600d2cb69e13f8399002300b85b92a
Harun Barış Bulut <baris.bulut@plusclouds.com>
2023-11-16 08:42:58 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 URL=$(printenv PLUSCLOUDS_API)
 SERIAL=$(dmidecode -s system-serial-number)
-META=$(curl -s $URL/v2/iaas/virtual-machines/meta-data?uuid=$SERIAL)
+META=$(curl -s $URL/v2/iaas/virtual-machines/meta-data?runner=bash&version=1&uuid=$SERIAL)
 
 PASSWORD=$(echo $META | jq -r '.data.password')
 HOSTNAME=$(echo $META | jq -r '.data.hostname')
